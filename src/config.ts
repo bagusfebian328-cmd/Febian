9e76c9d3894b0379ae2f7156ebf83b87a69f65d4
bagusfebian328-cmd <bagusfebian328@gmail.com>
2026-07-09 22:46:19 +0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,6 @@
 export const SUBTITLE = "Bagus Rizqy Febian"
 export const TITLE = "Student at SMP Kelas 8🎒"
 export const TAGLINE = "Welcome to my page! Kepoin media sosial aku di bawah ya ✨"
-
 // Contacts
 export const contact = "Hubungi aku"
 export const phone = "+6285876962829"
